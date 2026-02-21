694870601a6e59606e6d9606de81691ba87be118
Szabó Dániel Ernő <r3ap3rpy@gmail.com>
2026-02-21 20:25:17 +0100

    Renamed heap_sort to insertion sort

--- src/sorting/mod.rs
+++ src/sorting/mod.rs
@@ -8,5 +8,5 @@ pub use bead_sort::bead_sort;
 pub mod bubble_sort;
 pub use bubble_sort::bubble_sort;
 /// The `heap_sort` algorithm
-pub mod heap_sort;
-pub use heap_sort::heap_sort;
+pub mod insertion_sort;
+pub use insertion_sort::insertion_sort;
